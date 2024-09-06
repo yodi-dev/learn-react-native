cc95cda59ba2f99ce185f2552c53ea42036790d2
irawanyodi <dykhazta@gmail.com>
2024-09-06 19:39:44 +0700

    create: fitur choose foto

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,15 +1,16 @@
 import { StyleSheet, Image } from 'react-native';
 
-export default function ImageViewer({ placeholderImageSource }) {
-  return (
-    <Image source={placeholderImageSource} style={styles.image} />
-  );
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+    const imageSource = selectedImage ? { uri: selectedImage } : placeholderImageSource;
+
+    return <Image source={imageSource} style={styles.image} />;
 }
 
+
 const styles = StyleSheet.create({
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
 });
